a0f001f87169ab05a8aa92f61b0b7819fa2f7c42
Ignacio Lopez <ignacio.lopez@tenpo.cl>
2025-04-20 17:19:31 -0300

    update ci/cd

---
+++ server.js
@@ -0,0 +1,43 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const { Pool } = require('pg');
+
+const app = express();
+const port = 3000;
+const pool = new Pool({
+  user: 'postgres',
+  host: '192.168.1.192',
+  database: 'price_discovery',
+  password: 'postgres',
+  port: 5432,
+});
+// Servir archivos estáticos (HTML, JS, CSS, etc.)
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Redirigir cualquier ruta a index.html
+app.get('', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.get('/sucursales', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT sucursales_latitud, sucursales_longitud, sucursales_nombre FROM sucursales');
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).send(err.toString());
+  }
+});
+
+app.get('/sucursales2', async (req, res) => {
+  try {
+    const result = await pool.query("select * from vista_incrementos_sucursales;");
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).send(err.toString());
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
