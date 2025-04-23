e49f293df1f05ab42cf6d69d141ef9a86619578b
bylidev <contacto@byli.dev>
2025-04-23 01:02:05 -0300

    update names and labels

--- public/index.js
+++ public/index.js
@@ -30,8 +30,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
         radius: 50
       }).addTo(map)
         .bindPopup(`
-          <strong>Comercio:</strong> ${s.comercio_razon_social} <br>
-          <strong>Sucursal:</strong> ${s.sucursales_nombre} <br>
+          <strong>${s.sucursales_tipo} ${s.sucursales_nombre}</strong><br>
+          <strong>Razon social:</strong> ${s.comercio_razon_social} <br>
           <strong>Los precios promedio se desvian ${s.incremento_pct>100?"N/A":s.incremento_pct}% de los mejores precios del mercado.</strong><br>
           <strong>Variación respecto a ayer: </strong><span style="color:${pvarMin1<0?'red':'green'}">${pvarMin1}% ${pvarMin1 < 0 ? '📉' : '📈'}</span><br>
         `);
